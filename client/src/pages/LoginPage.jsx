bc10a65aa3ede15bb29dea8805b710fd0a978c97
Hamidaamir <hamidamir021@gmail.com>
2023-11-01 12:15:09 -0700

    validation

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -42,7 +42,7 @@ export default function LoginPage() {
             onChange={(ev) => setPassword(ev.target.value)}
           />
           <button className="primary">Login</button>
-          <div className="text-center py-2 text-gray-500">
+          <div className="text-center py-2 text-red-500">
             Don't have an account yet?{" "}
             <Link className="underline text-black" to={"/register"}>
               Register now
--- client/src/pages/Registerpage.jsx
+++ client/src/pages/Registerpage.jsx
@@ -6,19 +6,44 @@ export default function RegisterPage() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
   async function registerUser(ev) {
     ev.preventDefault();
-    try {
-      await axios.post("/register", {
-        name,
-        email,
-        password,
-      });
-      alert("Registration Successful. Now you can Login");
-    } catch (e) {
-      alert("Registration Failed. Please try again Later");
+
+    // Validation checks
+    let isValid = true;
+
+    if (!/^[a-zA-Z]+$/.test(name)) {
+      setNameError("Name must contain only alphabets.");
+      isValid = false;
+    } else {
+      setNameError("");
+    }
+
+    if (password.length < 8) {
+      setPasswordError("Password must be at least 8 characters.");
+      isValid = false;
+    } else {
+      setPasswordError("");
+    }
+
+    if (isValid) {
+      try {
+        await axios.post("/register", {
+          name,
+          email,
+          password,
+        });
+        alert("Registration Successful. Now you can Login");
+      } catch (e) {
+        alert("Registration Failed. Please try again Later");
+        console.log(e);
+      }
     }
   }
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
@@ -26,26 +51,37 @@ export default function RegisterPage() {
         <form className="max-w-md mx-auto " onSubmit={registerUser}>
           <input
             type="text"
-            placeholder="John Max"
+            placeholder="Name"
             value={name}
-            onChange={(ev) => setName(ev.target.value)}
+            onChange={(ev) => {
+              setName(ev.target.value);
+              setNameError(""); // Clear the error when input changes
+            }}
           />
+          {nameError && <div className="text-red-500">{nameError}</div>}
+
           <input
             type="email"
             placeholder="your@email.com"
             value={email}
             onChange={(ev) => setEmail(ev.target.value)}
           />
+
           <input
             type="password"
-            placeholder="password"
+            placeholder="Password"
             value={password}
-            onChange={(ev) => setPassword(ev.target.value)}
+            onChange={(ev) => {
+              setPassword(ev.target.value);
+              setPasswordError(""); // Clear the error when input changes
+            }}
           />
+          {passwordError && <div className="text-red-500">{passwordError}</div>}
+
           <button className="primary">Register</button>
           <div className="text-center py-2 text-gray-500">
             Already a member{" "}
-            <Link className="underline text-black" to={"/Login"}>
+            <Link className="underline text-black" to="/Login">
               Login
             </Link>
           </div>
