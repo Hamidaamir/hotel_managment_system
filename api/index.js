0a740face40c14d052d3a918dd62880635a207c8
FarooqQaiser <farooqqaiser679@gmail.com>
2023-11-11 00:26:18 +0500

    bugs fixed

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("./models/User.js");
 const cookieParser = require("cookie-parser");
+const imageDownloader = require("image-downloader");
 require("dotenv").config();
 const app = express();
 
@@ -102,6 +103,16 @@ app.get("/profile", (req, res) => {
 
 app.post("/logOut", (req, res) => {
   res.cookie("token", "").json(true);
+});
+
+app.post("/upload-by-link", async (req, res) => {
+  const { link } = req.body;
+  const newName = Date.now() + ".jpeg";
+  await imageDownloader.image({
+    url: link,
+    dest: __dirname + "/uploads" + newName,
+  });
+  res.json(newName);
 });
 
 app.listen(4000);
