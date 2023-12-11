c375955dd332b89568549912783d21833cf8bffa
FarooqQaiser <farooqqaiser679@gmail.com>
2023-12-11 22:01:07 +0500

    updated

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Indexpage() {
   const [places, setPlaces] = useState([]);
@@ -13,23 +14,25 @@ export default function Indexpage() {
     <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-col-3 lg:grid-cols-4">
       {places.length > 0 &&
         places.map((place) => (
-          <div key={place._id}>
-            <div className="bg-gray-500 mb-2 rounded-2xl flex">
-              {place.photos?.[0] && (
-                <img
-                  className="rounded-2xl object-cover aspect-square"
-                  src={"http://localhost:4000/uploads/" + place.photos?.[0]}
-                  alt=""
-                />
-              )}
-            </div>
+          <Link to={"/place/" + place._id}>
+            <div key={place._id}>
+              <div className="bg-gray-500 mb-2 rounded-2xl flex">
+                {place.photos?.[0] && (
+                  <img
+                    className="rounded-2xl object-cover aspect-square"
+                    src={"http://localhost:4000/uploads/" + place.photos?.[0]}
+                    alt=""
+                  />
+                )}
+              </div>
 
-            <h2 className="font-bold">{place.address}</h2>
-            <h3 className="text-sm truncate text-gray-500 ">{place.title}</h3>
-            <div className="mt-1">
-              <span className="font-bold">PKR:{place.price} </span>per night
+              <h2 className="font-bold">{place.address}</h2>
+              <h3 className="text-sm truncate text-gray-500 ">{place.title}</h3>
+              <div className="mt-1">
+                <span className="font-bold">PKR:{place.price} </span>per night
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
     </div>
   );
